67f253d90139f4b16c2fef9b4099d346bb16787d
DanyloHodz <danylohodz@gmail.com>
2025-08-14 15:26:45 +0300

    Add files via upload

---
+++ java5.java
@@ -0,0 +1,23 @@
+package проекти_відео;
+
+class java5 {
+
+    public static void main (String[] args){
+        //for (float i=100f; i > 0.5f; i/=2)
+            //System.out.println("Element is "+i);
+
+       /* int i =100;
+        while(i>=10) {
+            System.out.println("Element is "+i);
+            i-=10;
+        }*/
+
+
+        int i = 100;
+        do{
+            System.out.println("Element is "+i);
+            i++;
+        }while(i<10);
+    }
+
+}
